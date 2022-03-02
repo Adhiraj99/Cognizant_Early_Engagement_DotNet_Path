a8d60b5b13b9e490304dab256c76baed54275543
Adhiraj Das <69431252+Adhiraj99@users.noreply.github.com>
2022-03-02 08:35:01 +0530

    Update 18. No of time rented by each car.sql
    Function spacing causes error

--- Database Management/Hands-On/18. No of time rented by each car.sql
+++ Database Management/Hands-On/18. No of time rented by each car.sql
@@ -8,6 +8,6 @@ Write a query to display car id and number of times car taken for rental. Give a
 
 
 
-SELECT car_id, COUNT (car_id) AS NO_OF_TRIPS FROM rentals
+SELECT car_id, COUNT(car_id) AS NO_OF_TRIPS FROM rentals
 GROUP BY car_id
 ORDER BY car_id ASC;
